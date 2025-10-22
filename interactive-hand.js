d16889224ef750bda92e4492b020463f9cac25c4
danjrichards <dan@danrichards.co.uk>
2025-10-22 12:33:49 +0100

    Avoid script templates

--- interactive-hand.js
+++ interactive-hand.js
@@ -1,54 +1,28 @@
-// Helper function to strip unwanted tags from content
-function stripTags(content) {
-    return content.replace(/\n/g, '').replace(/<p>/g, '').replace(/<\/p>/g, '')
-}
-
 function updateText(id) {
-    const target = document.querySelector('.hand-pathways-content')
-    let content = ''
+    // Hide all content sections
+    document.querySelectorAll('.content-section').forEach(section => {
+        section.style.display = 'none'
+    })
 
     // Check for callout
     const callout = document.getElementById('hand-callout')
     if (callout) {
-        content += `<div class="hand-callout">${stripTags(callout.innerHTML)}</div>`
+        callout.style.display = 'block'
     }
 
     // Check if any region is highlighted
     if (document.querySelector("#svg_hands g g.highlight")) {
-        const template = document.getElementById(`t-${id}`)
-        content += stripTags(template.innerHTML)
+        const section = document.getElementById(`section-${id}`)
+        if (section) {
+            section.style.display = 'block'
+        }
     } else {
-        // Build table of contents
-        content += '<p class="meta-item">Click on a shape or one of the text links below to see appropriate pathways here.</p>'
-        content += '<ul>'
-        
-        const templates = document.querySelectorAll("script[type='text/template']")
-        templates.forEach(template => {
-            const parentId = template.id.substring(2) // Remove 't-' prefix
-            const templateContent = template.innerHTML
-            const tempDiv = document.createElement('div')
-            tempDiv.innerHTML = templateContent
-            
-            const heading = tempDiv.querySelector('h4')
-            const subheadings = Array.from(tempDiv.querySelectorAll('span'))
-                .map(span => span.innerHTML)
-
-            if (heading) {
-                content += `<li class="hands-toc">
-                    <a href="javascript:void(0)" onclick="textClickHandler('${parentId}')">${heading.innerHTML}</a>`
-                
-                if (subheadings.length > 0) {
-                    content += `<div class="subheadings">${subheadings.join('<br>')}</div>`
-                }
-                
-                content += '</li>'
-            }
-        })
-        
-        content += '</ul>'
+        // Show table of contents
+        const toc = document.getElementById('table-of-contents')
+        if (toc) {
+            toc.style.display = 'block'
+        }
     }
-
-    target.innerHTML = content
 }
 
 function clickHandler(group) {
